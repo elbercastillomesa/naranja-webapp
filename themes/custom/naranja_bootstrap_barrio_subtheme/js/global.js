d013543dfd6056e5830c28bc62365f3898c9f3eb
Elber Castillo Mesa <elbercastillomesa@gmail.com>
2024-12-03 10:36:06 -0500

    NWA:
    - Create a new Naranja Theme Subtheme using bootstrap_barrio as base Theme.
    - Replace CDN imports using local files:
     - js: //cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js -> js/bootstrap.bundle.min.js
     - css: //cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css -> css/bootstrap.min.css

---
+++ themes/custom/naranja_bootstrap_barrio_subtheme/js/global.js
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.naranja_bootstrap_barrio_subtheme = {
+    attach: function (context, settings) {
+
+    }
+  };
+
+})(Drupal);
